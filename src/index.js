a045730a17b6ab4b085088bfc1f9fffebe05f59c
Anton Verinov <git@zemlan.in>
2017-03-05 00:04:07 +0200

    drag’n’drop

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import m from 'mithril'
+import 'drag-drop-webkit-mobile'
+
+window.addEventListener('touchmove', () => {})
 
 const state = {
   order: [
@@ -24,6 +27,27 @@ const state = {
       id: '75337580-57AB-4248-8983-24BB88025970',
       text: ''
     },
+  },
+  draggedNoteId: null,
+}
+
+function moveNoteIntoPlaceOfAnother (noteId, anotherId) {
+  const noteIndex = state.order.indexOf(noteId)
+  const anotherIndex = state.order.indexOf(anotherId)
+  if (anotherIndex < noteIndex) {
+    return state.order
+      .slice(0, anotherIndex)
+      .concat([noteId])
+      .concat(state.order.slice(anotherIndex, noteIndex))
+      .concat(state.order.slice(noteIndex + 1))
+  } else if (anotherIndex > noteIndex) {
+    return state.order
+      .slice(0, noteIndex)
+      .concat(state.order.slice(noteIndex + 1, anotherIndex + 1))
+      .concat([noteId])
+      .concat(state.order.slice(anotherIndex + 1))
+  } else {
+    return state.order
   }
 }
 
@@ -33,13 +57,39 @@ const app = {
       {style: {
         width: '100%',
       }},
-      state.order.map(id => state.notes[id]).map(note =>
+      state.order.map(id => state.notes[id]).map((note, i) =>
         m('div',
-          {style: {
-            minHeight: '1em',
-            borderBottom: '1px solid lightgray',
-            display: 'flex',
-          }},
+          {
+            style: {
+              minHeight: '1em',
+              display: 'flex',
+              padding: '0.3em 0',
+              boxShadow: note.id === state.draggedNoteId ? '0 0 1em lightgray' : null,
+            },
+            ondragover: (e) => e.preventDefault(),
+            ondrop: e => {
+              if ([...e.dataTransfer.types].includes('text/x-note-id')) {
+                e.preventDefault()
+                e.dataTransfer.dropEffect = "move"
+                state.order = moveNoteIntoPlaceOfAnother(
+                  e.dataTransfer.getData('text/x-note-id'),
+                  note.id
+                )
+
+                state.draggedNoteId = null
+              }
+            },
+            ondragenter: e => {
+              if ([...e.dataTransfer.types].includes('text/x-note-id')) {
+                e.preventDefault()
+                e.dataTransfer.dropEffect = "move"
+                state.order = moveNoteIntoPlaceOfAnother(
+                  state.draggedNoteId,
+                  note.id
+                )
+              }
+            },
+          },
           m('textarea',
             {
               style: {
@@ -52,6 +102,7 @@ const app = {
                 outline: 'none',
                 resize: 'none',
                 padding: '1em',
+                backgroundColor: 'rgba(0,0,0,0)',
               },
               oninput: function (e) {
                 state.notes[note.id].text = e.target.value
@@ -60,17 +111,26 @@ const app = {
             },
             note.text
           ),
-          m('button',
+          m('div',
             {
+              draggable: true,
+              'data-note-id': note.id,
               style: {
                 flex: 'auto vertical',
                 width: '10%',
-                fontSize: '1em',
-                height: '2em',
+                fontSize: '3em',
                 alignSelf: 'center',
+                WebkitTapHighlightColor: 'rgba(0,0,0,0)',
+                textAlign: 'center',
               },
+              ondragstart: e => {
+                state.draggedNoteId = note.id
+                e.dataTransfer.setData('text/x-note-id', note.id)
+                e.dataTransfer.effectAllowed = 'move'
+              },
+              onclick: () => console.log('click')
             },
-            '='
+            '≡'
           )
         )
       )
